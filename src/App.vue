1fc36416b81b369e2d000dfbfcfa628b7cfda14f
PaulAjii <paulajii134@gmail.com>
2024-01-09 19:21:54 +0100

    Yet, another

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
   <h1>This is APS-UI</h1>
+
+  <HomePage />
+  <AboutPage />
 </template>
+
+<script setup>
+import HomePage from './components/Home.vue';
+import AboutPage from './components/About.vue';
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>This is the Home Page</h2>
+</template>

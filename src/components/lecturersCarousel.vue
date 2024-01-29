298487b3ab39442b809e838919010d1e54be4c05
Bihefcy <vicarowosafe@gmail.com>
2024-01-29 17:42:06 +0100

    added about us page

---
+++ src/components/lecturersCarousel.vue
@@ -0,0 +1,151 @@
+<template> 
+    <div class=" p-1 md:p-6 m-1 md:m-4 font-display bg-blue-100 rounded-md">
+      <div class="my-1">
+        <p class="text-4xl font-bold pl-4 pt-2">Our Lecturers</p>
+        <div class="flex items-center">
+          <div class="md:w-1/4 w-3/4 bg-aps-green h-[4px]"></div>
+          <div class="rounded-full h-4 w-4 bg-aps-orange"></div>
+        </div>
+      </div>
+  
+      <Carousel
+        v-bind="settings"
+        :autoplay="100000"
+        :pause-autoplay-on-hover="true"
+        :breakpoints="breakpoints"
+      >
+        <Slide
+          v-for="(data, index) in carouselData"
+          :key="index"
+        >
+          <div
+            class="w-full rounded-lg text-gray-950 text-xl bg-aps-orange "
+          >
+            <div class="h-[300px] w-full">
+              <img class="h-full w-full object-cover" :src= data.imgBg>
+            </div>
+            <div class="m-1 p-1 md:m-3">
+              <h3 class="md:text-lg text-[14px] font-semibold">{{ data.name}}</h3>
+              <p class="text-[12px] md:text-sm mb-1">{{ data.title }}</p>
+            </div>
+          </div>
+        </Slide>
+    
+        <template #addons>
+          <Pagination />
+        </template>
+      </Carousel>
+    </div>
+  
+  
+    </template>
+    
+    <script setup>
+    import { ref } from 'vue'
+    import { Carousel, Slide, Pagination } from 'vue3-carousel'
+    import 'vue3-carousel/dist/carousel.css'
+    
+    const carouselData = ref([
+      {
+        name: `Elijah Inioluwa`,
+        title: "President",
+        imgBg: 'src/assets/images/excos/exco1.jpg',
+      },
+      
+      {
+        name: `Oyekan Oluwatobi`,
+        title: "Vice President",
+        imgBg: 'src/assets/images/excos/exco2.png',
+      },
+    
+      {
+        name: `Olajide Ayomide`,
+        title: "General Secretary",
+        imgBg: 'src/assets/images/excos/exco3.jpg',
+      },
+  
+      {
+        name: `Adeniran Happiness`,
+        title: "Assistant General Secretary",
+        imgBg: 'src/assets/images/excos/exco4.jpg',
+      },
+  
+      {
+        name: `Mustapha Ogo-Oluwa`,
+        title: "Public Relations Officer",
+        imgBg: 'src/assets/images/excos/exco5.jpg',
+      },
+  
+      {
+        name: `Awopegba Jonadab`,
+        title: "Social Secretary",
+        imgBg: 'src/assets/images/excos/exco6.jpg',
+      },
+    
+      {
+        name: `Akinwola Oluwasegun`,
+        title: "Sports Secretary",
+        imgBg: 'src/assets/images/excos/exco7.jpg',
+      },
+  
+      {
+        name: `Moshood Babatunde`,
+        title: "Financial Secretary",
+        imgBg: 'src/assets/images/excos/exco8.jpg',
+      },
+  
+      {
+        name: `Olanrewaju Timileyin`,
+        title: "Treasurer",
+        imgBg: 'src/assets/images/excos/exco9.jpg',
+      },
+  
+      {
+        name: `Adebayo Samuel`,
+        title: "Special Duties Officer",
+        imgBg: 'src/assets/images/excos/exco10.jpg',
+      },
+  
+      {
+        name: `Solomon Oluwatodimu`,
+        title: "Senate President",
+        imgBg: 'src/assets/images/excos/exco11.jpg',
+      },
+  
+      {
+        name: `Goodluck Omotayo`,
+        title: "Deputy Senate President",
+        imgBg: 'src/assets/images/excos/exco12.jpg',
+      },
+  
+      {
+        name: `Egun Jane`,
+        title: "Senate Clerk",
+        imgBg: 'src/assets/images/excos/exco13.jpg',
+      },
+    ])
+  
+    const settings = ref({
+    itemsToShow: 1.5,
+    snapAlign: 'center',
+     });
+    const breakpoints = ref({
+      768: {
+        itemsToShow: 2.5,
+        snapAlign: 'center',
+      },
+    });
+    defineProps(['settings', 'breakpoints']);
+      </script>
+    
+    <style scoped>
+    .carousel__slide {
+      padding: 10px;
+    }
+    
+    .carousel__prev,
+    .carousel__next {
+      box-sizing: content-box;
+      border: 5px solid white;
+    }
+    </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,75 @@
 <template>
-    <h2>This is the About Page</h2>
+    <div class="font-display">
+<!-- This is the ABOUT US section beginning--->
+        <div class="bg-gray-200 md:flex md:justify-between md:items-center gap-4 p-4">
+            <div class="md:w-3/4">
+                <p class="text-3xl font-extrabold text-center md:text-left mb-4">About Us</p>
+                <p class="text-center md:text-left">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure eius suscipit obcaecati veniam quae possimus mollitia ad id? Explicabo facilis animi quos omnis quod unde excepturi quisquam, iste odio architecto veniam reiciendis alias consequatur obcaecati quam! Esse ratione consectetur quos recusandae libero, dignissimos cumque eum quia? Doloribus, reprehenderit. Porro eius aliquam voluptates quas odit cumque, temporibus laudantium in maxime recusandae officia! Harum animi pariatur voluptate delectus tempore! Iste doloremque, repudiandae, quasi perferendis ipsa architecto earum nemo consequuntur qui asperiores ducimus magni. Facilis asperiores ipsum quos minima doloremque, veniam, accusantium provident beatae debitis alias omnis voluptates neque unde, rerum eligendi voluptatibus. Aut nesciunt eligendi et voluptatibus deleniti saepe, sed enim tenetur quisquam? Quo alias accusamus numquam aperiam, ducimus quaerat minus, dignissimos quis asperiores incidunt nisi veritatis mollitia harum porro fugiat sequi delectus natus vel nam perferendis sint quia. Inventore perferendis, quaerat itaque rerum eaque repellat quidem, sapiente debitis temporibus fugiat praesentium!
+                </p>
+            </div>
+            <div class="h-full md:w-3/4">
+                <img class="h-full w-full object-cover" :src= "apsBuilding" />
+            </div>
+        </div>
+<!-- This is the ABOUT US section end--->
+
+<!-- This is the ABOUT THE LOGO section beginning--->
+        <div class="bg-green-200 md:flex md:justify-between md:items-center gap-4 p-4 m-8">
+            <div class="h-[150px] md:w-3/4 md:pb-0 pb-4">
+                <img class= "h-full object-cover mx-auto" :src= "apsLogo" />
+            </div>            
+            <div class="md:w-3/4">
+                <p class="text-3xl font-extrabold text-center md:text-right mb-4">About the Logo</p>
+                <p class="text-center md:text-right">
+                    The logo of the Association shall consist of a snake coiling round a crutch that is 
+positioned between a hand and a spark. The snake stands for the oneness of the medical 
+profession. The crutch symbolizes ambulation, the hand stands for massage and exercise 
+therapy and the spark symbolizes electrotherapy.
+
+                </p>
+            </div>
+
+        </div>
+<!-- This is the ABOUT OUR LOGO section end--->
+
+
+<!-- This is the OUR AIMS section beginning--->        
+        <div class="bg-yellow-200 md:flex md:justify-between md:items-center gap-4 m-8 p-4">
+            <div class="md:w-3/4">
+                <p class="text-3xl font-extrabold text-center md:text-left mb-4">Our Aims</p>
+                <ul class="text-center md:text-left md:pb-0 pb-6">
+                    <li>1. To protect the rights and privileges of her members.</li> 
+                    <li>2. To create awareness of the profession among the general public.</li> 
+                    <li>3. To promote cordial relationship with other student bodies within and outside the country whose objectives are acceptable to the Association.</li>
+                    <li>4. To ensure mutual interactions amongst members.</li> 
+                    <li>5. To create awareness among members about the latest trend in physiotherapy practice in 
+                    Nigeria and the rest of the world.</li>
+                    <li>6. To organize activities that will harmonize the pre-clinical and clinical students.</li>
+                </ul>
+            </div>
+            <div class="h-full md:w-3/4">
+                <img class="h-full w-full object-cover" :src= "apsBuilding" />
+            </div>
+        </div>
+<!-- This is the OUR AIMS section end--->
+
+<!-- This is the OUR MOTTO section beginning--->
+        <div class="bg-gray-200 m-8 p-4">
+            <p class="text-3xl font-extrabold text-center mb-4">Our Motto</p>
+            <p class="text-center">TO SERVE MANKIND AND HONOUR GOD</p>
+        </div>
+<!-- This is the OUR MOTTO section end--->
+<lecturersCarousel />
+    </div>
+
+
+
 </template>
+<script setup>
+    import apsBuilding from '@/assets/images/DSC_0450.jpg'
+    import apsLogo from '@/assets/images/aps-logo.png'
+    import lecturersCarousel from "../components/lecturersCarousel.vue"
+
+
+</script>

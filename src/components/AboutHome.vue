542d99e972c0f80c401ca826c0642e4189f4be55
PaulAjii <paulajii134@gmail.com>
2024-02-10 14:30:23 +0100

    Added Pinia; Worked on AboutHome

--- src/components/AboutHome.vue
+++ src/components/AboutHome.vue
@@ -1,5 +1,20 @@
 <template>
-    <section>
-        <h3>This is the about section</h3>
+    <section class="p-1 md:p-6 font-display my-8 ml-4 md:m-8 bg-green-100 rounded-md">
+        <div class="my-1">
+        <p class="text-4xl font-bold pl-4 pt-2">About Us</p>
+        <div class="flex items-center pl-4">
+            <div class="md:w-1/4 w-3/4 bg-aps-green h-[4px]"></div>
+            <div class="rounded-full h-4 w-4 bg-aps-orange"></div>
+        </div>      
+        </div>
+
+        <div class="flex item-start flex-column mx-4">
+            <p class="about-us__text basis-3/5">
+                Physiotherapy, also known as physical therapy, is a healthcare profession that focuses on optimizing human movement and function. It involves the assessment, diagnosis, and treatment of various physical conditions and injuries to enhance an individual's overall well-being. <br> <br>
+                Physiotherapists, or physical therapists, use a combination of manual therapy, exercise prescription, education, and other therapeutic modalities to help patients...
+            </p>
+
+            <div class="img__block">jdjdj</div>
+        </div>
     </section>
 </template>

2eba6d8704b38f58a59cb62220c4a86fe7c91bab
Bihefcy <vicarowosafe@gmail.com>
2024-02-16 18:28:36 +0100

    alumni

---
+++ src/components/PastPresidents.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="p-1 md:p-6 font-display my-8 mx-4 md:m-8 bg-yellow-200 rounded-md">
+        <div class="">
+            <p class="font-bold text-center text-2xl md:text-4xl ">Past Presidents</p>
+        </div>
+        <div class=" p-1 md:p-6 my-8 mx-4 md:m-8 text-base md:text-2xl">
+            <table class="table-fixed border-separate border-spacing-2 border border-slate-100 mx-auto w-full text-center">
+                <thead>
+                    <tr>
+                    <th class="border border-slate-200">Name</th>
+                    <th class="border border-slate-200">Year</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(item, index) in tableData" :key="index">
+                        <td class="border border-slate-200">{{ item.name }}</td> <td class="border border-slate-200">{{ item.tenure }}</td>
+                    </tr>
+                    <tr>
+                    <td class="border border-slate-200">Witchy Woman</td>
+                    <td class="border border-slate-200">1972</td>
+                    </tr>
+                    <tr>
+                    <td class="border border-slate-200">Shining Star</td>
+                    <td class="border border-slate-200">1975</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        
+    </div>
+</template>
+
+<script setup>
+// Import any required libraries, e.g., for date formatting
+
+// Define your data array
+const tableData = [
+  { name: 'John Doe', tenure: 30 },
+  { name: 'Jane Smith', tenure: 25 },
+  // Add more objects as needed
+];
+
+// Additional functions for formatting, sorting, etc. (optional)
+</script>
--- src/views/Alumni.vue
+++ src/views/Alumni.vue
@@ -16,18 +16,18 @@
 
           </div>
           <div class="col-span-2 p-1 w-full rounded-md ">
-            <p class="m-1 text-center font-bold text-6xl md:text-9xl lg:text-[220px]"> Our Alumni </p>
+            <p class="m-1 text-center font-bold text-6xl md:text-9xl lg:text-[200px] xl:text-[238px]"> Our Alumni </p>
           </div>
-          <div class=" w-full h-[120px] md:h-[270px] bg-red-400 rounded-md">
+          <div class=" w-full h-[120px] md:h-[270px] xl:h-[460px] bg-red-400 rounded-md">
             <img class="h-full w-full object-cover rounded-md" :src= "a1" />
 
           </div>
           <div class=" w-full bg-red-400 rounded-md h-[120px] md:h-[280px]">
-            <img class="h-full w-full object-cover rounded-md" :src= "a2" />
+            <img class="h-full w-full object-cover rounded-md" :src= "a3" />
 
           </div>
           <div class="w-full h-[120px] md:h-[280px] bg-red-400 rounded-md">
-            <img class="h-full w-full object-cover rounded-md" :src= "a2" />
+            <img class="h-full w-full object-cover rounded-md" :src= "a1" />
 
           </div>
           <div class="w-full h-[120px] md:h-[280px] bg-red-400 rounded-md">
@@ -36,7 +36,8 @@
           </div>
         </div>
       </div>
-      
+      <AlumniGalleryCarousel />
+      <PastPresidents />
     </div>
 
 
@@ -46,4 +47,6 @@
     import a1 from '@/assets/images/DSC_0457.jpg'
     import a2 from '@/assets/images/IMG_0848.jpg'
     import a3 from '@/assets/images/IMG_0850.jpg'
+    import AlumniGalleryCarousel from '../components/AlumniGalleryCarousel.vue';
+    import PastPresidents from '../components/PastPresidents.vue';
 </script>

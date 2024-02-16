8551ee755c9d187d1d2113f2aa461d16c162a320
Bihefcy <vicarowosafe@gmail.com>
2024-02-16 21:32:09 +0100

    alumni

--- src/components/PastPresidents.vue
+++ src/components/PastPresidents.vue
@@ -15,14 +15,6 @@
                     <tr v-for="(item, index) in tableData" :key="index">
                         <td class="border border-slate-200">{{ item.name }}</td> <td class="border border-slate-200">{{ item.tenure }}</td>
                     </tr>
-                    <tr>
-                    <td class="border border-slate-200">Witchy Woman</td>
-                    <td class="border border-slate-200">1972</td>
-                    </tr>
-                    <tr>
-                    <td class="border border-slate-200">Shining Star</td>
-                    <td class="border border-slate-200">1975</td>
-                    </tr>
                 </tbody>
             </table>
         </div>
@@ -35,8 +27,42 @@
 
 // Define your data array
 const tableData = [
-  { name: 'John Doe', tenure: 30 },
-  { name: 'Jane Smith', tenure: 25 },
+    { name: 'Egbedeyi A.S' , tenure: '1968/1969' },
+    { name: 'Adefihan T.A' , tenure:  '1976/1977' },
+    { name: 'Abegunde A.' , tenure: '1982/1983' },
+    { name: 'Jaiyesimi A.O' , tenure: '1983/1984' },
+    { name: 'Bello O.M' , tenure: '1984/1985' },
+    { name: 'Momah D.N' , tenure: '1985/1986'},
+    { name: 'Ayanniyi O.' , tenure: '1986/1987' },
+    { name: 'Osuamadi A.C' , tenure: '1987/1988' },
+    { name: 'Toweh P.O' , tenure: '1989/1990' },
+    { name: 'Onyekwere I.L' , tenure: '1990/1991' },
+    { name: 'Adebari O.A' , tenure: '1991/1992' },
+    { name: 'Aluko J.O' , tenure: '1992/1993' },
+    { name: 'Adebiyi R.A' , tenure: '1995/1996' },
+    { name: 'Jegede F.B' , tenure: '1996/1997' },
+    { name: 'Adeniyi A.F' , tenure: '1997/1998' },
+    { name: 'Aba S.O.F' , tenure: '1998/1999' },
+    { name: 'Njoku S.O.N' , tenure: '2000/2001' },
+    { name: 'Mordi U.B' , tenure: '2002/2003' },
+    { name: 'Kareem R.O' , tenure: '2003/2004' },
+    { name: 'Odunowo O.M' , tenure: '2004/2005' },
+    { name: 'Adewunmi I.O' , tenure: '2005/2006' },
+    { name: 'Agbomeji O.O' , tenure: '2007/2008' },
+    { name: 'Salam O.M' , tenure: '2008/2009' },
+    { name: 'Obidiegwu C.J' , tenure: '2009/2010' },
+    { name: 'Dosa G.T.B' , tenure: '2010/2011' },
+    { name: 'Agbomeji O.T' , tenure: '2011/2012' },
+    { name: 'Fadero O.S' , tenure: '2012/2013' },
+    { name: 'Aba S.S' , tenure: '2013/2014' },
+    { name: 'Olasanoye O.T' , tenure: '2014/2015' },
+    { name: 'Ekwueme C.C' , tenure: '2015/2016'},
+    { name: 'Oni O.G' , tenure: '2016/2017' },
+    { name: 'Gbonjubola Y.T' , tenure: '2017/2018' },
+    { name: 'Oluwatayo Damilola S.' , tenure: '2019/2021' },
+    { name: 'Adetunji Harrison' , tenure: '2021/2022' },
+    { name: 'Adeyemo Dorcas A.' , tenure: '2022/2023' },
+
   // Add more objects as needed
 ];
 
--- src/views/Alumni.vue
+++ src/views/Alumni.vue
@@ -38,6 +38,37 @@
       </div>
       <AlumniGalleryCarousel />
       <PastPresidents />
+      
+      <div class="p-1 md:p-6 font-display my-8 mx-4 md:m-8 bg-green-200 rounded-md">
+        <div class="">
+            <p class="font-bold text-center text-2xl md:text-4xl ">Message to Our Alumni</p>
+        </div>
+        <p class="p-1">Dear Esteemed Alumni,
+                    <br/>
+                    <br/>
+          It is with great joy and pride that we extend a warm welcome to each and every one of you, our valued alumni, who have played an integral role in shaping the rich tapestry of our school's history.
+          <br/>
+          <br/>
+          Your journey with us marked not just the completion of your academic pursuits but the beginning of a lifelong connection with a community that cherishes its members. As you navigate through this dedicated alumni space, we invite you to reminisce, reconnect, and rediscover the bonds that tie us together.
+          <br/>
+          <br/>
+          Your achievements, both personal and professional, are a testament to the resilience, intellect, and character that define our alumni. We take immense pride in witnessing your successes and celebrate the diverse paths each one of you has chosen.
+          <br/>
+          <br/>
+          This platform is not just a digital space; it's a testament to the enduring spirit of our school community. Here, you'll find a wealth of resources, from career opportunities to the latest happenings within the school. We encourage you to explore, contribute, and stay engaged as we continue to grow and evolve.
+          <br/>
+          <br/>
+          Your stories, experiences, and continued involvement are what make our school's legacy vibrant and enduring. Whether you graduated recently or several years ago, you remain an integral part of our ever-expanding family.
+          Thank you for being an essential part of our journey. We look forward to fostering a deeper connection with you and creating more memories together.
+          <br/>
+          <br/>
+          Welcome back home!
+          <br/>
+          Warm regards,
+          <br/>
+          [APS] Alumni Relations Team</p>
+      </div>
+
     </div>
 
 

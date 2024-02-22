cff6c64c122b018b3aa02cd2b5d4e1f027761558
PaulAjii <paulajii134@gmail.com>
2024-02-22 18:20:22 +0100

    Worked on the Profile...

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,20 +1,44 @@
 <template>
     <div class="w-[95%] mx-auto h-full">
         <h2 class="my-4 font-semibold text-aps-green">
-                        Profile
+                        Profile Information
         </h2>
 
-        <div class="bg-gray-100">
-            <div>
-                <label for="">
-                    Full Name:
+        <div class="shadow py-4 px-2">
+            <!-- First Parameter -->
+            <div class="mb-px">
+                <label class="font-bold text-[13px] text-gray-400">
+                    Full Name
                 </label>
-                <div>
-                    <span></span>
-                    <input
-                        type="text"
-                        readonly :value="fullName"
-                    />
+                <div class="flex gap-2 items-center border border-gray-100">
+                    <span class="text-gray-400 border border-gray-100 px-2">
+                        <i class="bi bi-person text-2xl"></i>
+                    </span>
+                    <div class="text-gray-400 font-semibold">{{ fullName }}</div>
+                </div>
+            </div>
+            <!-- Second Parameter -->
+            <div class="mb-px">
+                <label class="font-bold text-[13px] text-gray-400">
+                    Matric Number
+                </label>
+                <div class="flex gap-2 items-center border border-gray-100">
+                    <span class="text-gray-400 border border-gray-100 px-2">
+                        <i class="bi bi-fingerprint text-2xl"></i>
+                    </span>
+                    <div class="text-gray-400 font-semibold">{{ store.user.matricNumber }}</div>
+                </div>
+            </div>
+            <!-- Third Parameter -->
+            <div class="mb-px">
+                <label class="font-bold text-[13px] text-gray-400">
+                    Level
+                </label>
+                <div class="flex gap-2 items-center border border-gray-100">
+                    <span class="text-gray-400 border border-gray-100 px-2">
+                        <i class="bi bi-person-vcard text-2xl"></i>
+                    </span>
+                    <div class="text-gray-400 font-semibold">{{ store.user.level }}</div>
                 </div>
             </div>
         </div>

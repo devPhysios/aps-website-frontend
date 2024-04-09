d489b5eda137a7506067a85f02ae98ba29d0961a
PaulAjii <paulajii134@gmail.com>
2024-04-09 09:21:28 +0100

    Dashboard: Changed to yellow book

--- src/components/DashBoardCards.vue
+++ src/components/DashBoardCards.vue
@@ -4,7 +4,7 @@
         <div
             class="shadow rounded rounded-tl-lg rounded-tr-lg sm:rounded-tr-none group relative bg-white p-6 focus-within:ring-inset focus-within:ring-indigo-500">
             <div>
-                <span class="inline-flex rounded-lg p-3 bg-teal-50 text-teal-700 ring-white">
+                <span class="inline-flex rounded-lg p-3 bg-blue-50 text-blue-700 ring-white">
                     <svg class="w-6 h-6" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round"
                         stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -66,7 +66,7 @@
         <div
             class="shadow rounded sm:rounded-tl-lg sm:rounded-tr-none group relative bg-white p-6 focus-within:ring-inset focus-within:ring-indigo-500">
             <div>
-                <span class="inline-flex rounded-lg p-3 bg-teal-50 text-teal-700 ring-white">
+                <span class="inline-flex rounded-lg p-3 bg-orange-50 text-aps-orange ring-white">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" class="bi bi-book w-6 h-6" viewBox="0 0 18 18" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
                             d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783"/>
@@ -96,7 +96,7 @@
         <div
             class="shadow rounded sm:rounded-tr-lg sm:rounded-tr-none group relative bg-white p-6 focus-within:ring-inset focus-within:ring-indigo-500">
             <div>
-                <span class="inline-flex rounded-lg p-3 bg-teal-50 text-teal-700 ring-white">
+                <span class="inline-flex rounded-lg p-3 bg-yellow-50 text-yellow-700 ring-white">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" class="bi bi-journal w-6 h-6" viewBox="0 0 18 18" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
                             d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2"/>
@@ -109,7 +109,7 @@
                 <h3 class="text-base font-semibold leading-6 text-gray-900">
                     <RouterLink to="#" class="focus:outline-none">
                         <span class="absolute inset-0" aria-hidden="true"></span>
-                        Orange Book
+                        Yellow Book
                     </RouterLink>
                 </h3>
                 <p class="mt-2 text-sm text-gray-500">

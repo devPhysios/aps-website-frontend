418a3d463f66e8687baf64be61c3f06640fe6e8c
FeranmiDev <macguzik98@gmail.com>
2024-04-21 17:04:09 +0100

    Refactor navigation and questions views, and update dashboard styling

--- src/views/Levelquestions.vue
+++ src/views/Levelquestions.vue
@@ -14,11 +14,11 @@
     </nav>
     <div class="grid grid-cols md:grid-cols-3 lg:grid-cols-4 gap-6 m-5">
         <div v-for="course in courses" :key="course.coursecode"
-            class="p-6 bg-yellow-200 rounded-lg shadow-md hover:shadow-xl transform transition duration-300 ease-in-out hover:scale-105 cursor-pointer hover:bg-green-500">
+            class="p-6 bg-amber-600 rounded-lg shadow-md hover:shadow-xl transform transition duration-300 ease-in-out hover:scale-105 cursor-pointer hover:bg-green-500">
             <router-link :to="`/dashboard/questions/${level}/${course.coursecode}`">
                 <div class="text-center">
                     <h2 class="text-xl font-semibold mb-2 text-gray-800">{{ course.coursecode }}</h2>
-                    <p class="text-gray-600">{{ course.coursetitle }}</p>
+                    <p class="text-white">{{ course.coursetitle }}</p>
                 </div>
             </router-link>
         </div>

ecb0297b3ce96957de2c6182949d6dd20c6c73b5
FeranmiDev <macguzik98@gmail.com>
2024-04-22 16:11:14 +0100

    Remove duplicate route for index.html in router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -29,11 +29,6 @@ const routes = [
     component: Home,
   },
   {
-    path: "/index.html",
-    name: "HomePage",
-    component: Home,
-  },
-  {
     path: "/about",
     name: "AboutPage",
     component: About,
@@ -157,7 +152,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 

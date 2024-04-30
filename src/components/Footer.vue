41541fbcb56214732b3956d1f217ea6d6db22bc6
FeranmiDev <macguzik98@gmail.com>
2024-04-30 23:11:53 +0100

    Refactor HeroCarousel.vue component and add alumni images

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,101 +1,91 @@
 <template>
-    <footer class="bg-[#450a0a] font-display">
-        <p> {{ footerText }}</p>
-        <div class="md:flex ">
-            <div class="flex-1 p-5">
-                <strong>
-                    <h1 class="underline-green-500 text-white">
-                        Our Address</h1>
-                </strong>
-                <div class="flex items-center">
-                    <div class="w-1/4 h-1 bg-green-400"></div>
-                    <div class="w-3 h-3 bg-yellow-400 rounded-full"></div>
-                </div>
-
-
-                <div class="flex items-center">
-                    <div class="flex items-center gap-4">
-                        <div><i class="bi bi-geo-alt-fill text-white"></i></div>
-                        <p class="text-white">Room EE, Alexander Brown Hall, University College Hospital, Ibadan.
-                        </p>
-                    </div>
-                </div>
-                <div class="flex items-center">
-                    <div class="flex items-center gap-4">
-                        <div><i class="bi bi-envelope-open-fill text-white"></i></div>
-                        <p class="text-white"> &nbsp;apsuiibadan@gmail.com</p>
-                    </div>
-                </div>
-                <div class="flex items-center">
-                    <div class="flex items-center gap-4">
-                        <div><i class="bi bi-telephone-fill text-white"></i></div>
-                        <p class="text-white">+234 81213080920</p>
-                    </div>
-                </div>
-            </div>
-            <div class="flex-wrap ">
-                <div class="flex-1 p-5">
-                    <div class="text-white">
-                        <!-- <p> -->
-                            <strong>
-                                <h1>Quick Links </h1>
-                            </strong>
-                            <div class="flex items-center">
-                                <div class="w-1/4 h-1 bg-green-400"></div>
-                                <div class="w-3 h-3 bg-yellow-400 rounded-full"></div>
-                            </div>
-                        <ul class="list-none">
-                            <li>
-                                <RouterLink to="/">
-                                    Home
-                                </RouterLink>    
-                                </li>
-                            <li>
-                                <RouterLink to="/about">
-                                    About
-                                </RouterLink>
-                            </li>
-                            <li>
-                                <RouterLink to="/gallery">
-                                    Gallery
-                                </RouterLink>
-                            </li>
-                            <li>
-                                <a href="https://physiopressui.wordpress.com/home/" target="_blank">PhysioPress</a>
-                            </li>
-                            <li>
-                                <RouterLink to="/alumni">
-                                    Alumni
-                                </RouterLink>
-                            </li>
-                        </ul>
-                        <!-- </p> <br> -->
-                        <div class="flex">
-                            <div class="flex-1 w-5 h-5 mr-6">
-                            </div>
-                            <div class="contents">
-                                <div class="flex-1 w-5 h-5 mr-6">
-                                </div>
-                            </div>
-                            <div class="flex-1 w-5 h-5 "></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <footer class="bg-[#450a0a] font-display">
+    <p>{{ footerText }}</p>
+    <div class="md:flex">
+      <div class="flex-1 p-5">
+        <strong>
+          <h1 class="underline-green-500 text-white">Our Address</h1>
+        </strong>
+        <div class="flex items-center">
+          <div class="w-1/4 h-1 bg-green-400"></div>
+          <div class="w-3 h-3 bg-yellow-400 rounded-full"></div>
         </div>
 
-    </footer>
+        <div class="flex items-center">
+          <div class="flex items-center gap-4">
+            <div><i class="bi bi-geo-alt-fill text-white"></i></div>
+            <p class="text-white">
+              Room EE, Alexander Brown Hall, University College Hospital,
+              Ibadan.
+            </p>
+          </div>
+        </div>
+        <div class="flex items-center">
+          <div class="flex items-center gap-4">
+            <div><i class="bi bi-envelope-open-fill text-white"></i></div>
+            <p class="text-white">&nbsp;apsibadan@gmail.com</p>
+          </div>
+        </div>
+        <div class="flex items-center">
+          <div class="flex items-center gap-4">
+            <div><i class="bi bi-telephone-fill text-white"></i></div>
+            <p class="text-white">+234 9165305415</p>
+          </div>
+        </div>
+      </div>
+      <div class="flex-wrap">
+        <div class="flex-1 p-5">
+          <div class="text-white">
+            <!-- <p> -->
+            <strong>
+              <h1>Quick Links</h1>
+            </strong>
+            <div class="flex items-center">
+              <div class="w-1/4 h-1 bg-green-400"></div>
+              <div class="w-3 h-3 bg-yellow-400 rounded-full"></div>
+            </div>
+            <ul class="list-none">
+              <li>
+                <RouterLink to="/"> Home </RouterLink>
+              </li>
+              <li>
+                <RouterLink to="/about"> About </RouterLink>
+              </li>
+              <li>
+                <RouterLink to="/gallery"> Gallery </RouterLink>
+              </li>
+              <li>
+                <a
+                  href="https://physiopressui.wordpress.com/home/"
+                  target="_blank"
+                  >PhysioPress</a
+                >
+              </li>
+              <li>
+                <RouterLink to="/alumni"> Alumni </RouterLink>
+              </li>
+            </ul>
+            <!-- </p> <br> -->
+            <div class="flex">
+              <div class="flex-1 w-5 h-5 mr-6"></div>
+              <div class="contents">
+                <div class="flex-1 w-5 h-5 mr-6"></div>
+              </div>
+              <div class="flex-1 w-5 h-5"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
-<script >
+<script>
 export default {
-    name: 'Footer',
-    setup() {
-        return {
-            footerText: ''
-
-        }
-    }
-}
+  name: "Footer",
+  setup() {
+    return {
+      footerText: "",
+    };
+  },
+};
 </script>
-
-
--- src/components/HeroCarousel.vue
+++ src/components/HeroCarousel.vue
@@ -1,40 +1,49 @@
 <template>
   <div class="pt-[70px]">
-  <Carousel :wrap-around="true" :autoplay="10000" :pause-autoplay-on-hover="true">
-    <Slide v-for="(data, index) in carouselData" :key="index">
+    <Carousel
+      :wrap-around="true"
+      :autoplay="10000"
+      :pause-autoplay-on-hover="true"
+    >
+      <Slide v-for="(data, index) in carouselData" :key="index">
+        <div
+          class="flex flex-col justify-center items-center w-full min-h-[80vh] text-aps-white gap-2 font-display"
+          :style="{
+            background: `#555 url(${data.imgBg}) no-repeat center/cover`,
+            backgroundBlendMode: 'multiply',
+          }"
+        >
+          <h3 class="text-xl mb-3">{{ data.text.toUpperCase() }}</h3>
+          <p class="mb-4">{{ data.subtitle }}</p>
+          <router-link :to="data.link">
+            <button
+              class="bg-aps-green text-aps-white rounded-md hover:bg-aps-orange px-3 py-3"
+            >
+              {{ data.ctaBtn }}
+            </button>
+          </router-link>
+        </div>
+      </Slide>
 
-      <div class="flex flex-col justify-center items-center w-full min-h-[80vh] text-aps-white  gap-2 font-display"
-        :style="{ background: `#555 url(${data.imgBg}) no-repeat center/cover`, backgroundBlendMode: 'multiply' }">
-        <h3 class="text-xl mb-3">{{ data.text.toUpperCase() }}</h3>
-        <p class="mb-4">{{ data.subtitle }}</p>
-        <router-link :to='data.link'>
-          <button class="bg-aps-green text-aps-white  rounded-md hover:bg-aps-orange px-3 py-3">
-            {{ data.ctaBtn }}
-          </button>
-        </router-link>
-      </div>
-
-    </Slide>
-
-    <!-- <template #addons>
+      <!-- <template #addons>
       <Pagination />
     </template> -->
-  </Carousel>
+    </Carousel>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { Carousel, Slide, Pagination } from 'vue3-carousel';
-import 'vue3-carousel/dist/carousel.css';
-import { RouterLink } from 'vue-router';
+import { ref } from "vue";
+import { Carousel, Slide, Pagination } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
+import { RouterLink } from "vue-router";
 
-import img500 from "@/assets/images/500l.jpg"
-import img400 from "@/assets/images/400l.jpg"
-import img100 from "@/assets/images/100l.jpg"
-import img200 from "@/assets/images/200l.jpg"
-import img300 from "@/assets/images/300l.jpg"
-import img1 from "@/assets/images/1.jpg"
+import img500 from "@/assets/images/500l.jpg";
+import img400 from "@/assets/images/400l.jpg";
+import img100 from "@/assets/images/100l.jpg";
+import img200 from "@/assets/images/200l.jpg";
+import img300 from "@/assets/images/300l.jpg";
+import img1 from "@/assets/images/1.jpg";
 
 const carouselData = ref([
   {
@@ -42,7 +51,7 @@ const carouselData = ref([
     subtitle: "Welcome To Our Official Website",
     imgBg: img1,
     ctaBtn: "About Us",
-    link: "/about"
+    link: "/about",
   },
 
   {
@@ -50,7 +59,7 @@ const carouselData = ref([
     subtitle: "Explore Our Vibrant Community",
     imgBg: img100,
     ctaBtn: "Explore Gallery",
-    link: "/gallery"
+    link: "/gallery",
   },
 
   {
@@ -58,7 +67,7 @@ const carouselData = ref([
     subtitle: "Pay Your Fees, Access Materials and Past Questions",
     imgBg: img400,
     ctaBtn: "Student Portal",
-    link: "/dashboard"
+    link: "/dashboard",
   },
 
   {
@@ -66,24 +75,26 @@ const carouselData = ref([
     subtitle: "Students Association's Press Website",
     imgBg: img300,
     ctaBtn: "PhysioPress",
-    link: "https://physiopressui.wordpress.com/home/"
+    link: "https://www.physiopressui.wordpress.com/home/",
   },
 
   {
     text: "PhysioRay",
-    subtitle: "Explore our association's journal repository from insightful student perspectives.",
+    subtitle:
+      "Explore our association's journal repository from insightful student perspectives.",
     imgBg: img200,
     ctaBtn: "PhysioRay",
-    link: "https://christnestteens.com/physioray/"
+    link: "https://christnestteens.com/physioray/",
   },
 
   {
-    text: "OUR FINALISTS",
-    subtitle: "",
+    text: "Alumni Spotlight Page",
+    subtitle: "Capturing Moments, Sharing Messages and Empowering Futures: Alumni Giving Back",
     imgBg: img500,
-    link: ""
+    ctaBtn: "Alumni",
+    link: "/alumni",
   },
-])
+]);
 </script>
 
 <style scoped>

41567eadb432953d97380cbbb76c5f96db78b382
FeranmiDev <macguzik98@gmail.com>
2024-06-28 20:33:15 +0100

    refactor: Update token validation to use "studentToken" instead of "token"

--- src/App.vue
+++ src/App.vue
@@ -8,51 +8,16 @@
 
 <script setup>
 import FooterMenu from "./components/Footer.vue";
-import NavigationMenu from "./components/Navigation.vue"
+import NavigationMenu from "./components/Navigation.vue";
 import { useUserStore } from "./stores/UserStore";
-import axios from 'axios';
 
-import { computed, onMounted } from "vue";
+import { computed } from "vue";
 
-const store = useUserStore()
-const displayFooter = computed(() => store.displayFooter)
-const displayHeader = computed(() => store.displayHeader)
-// Token validation function
-const validateToken = async () => {
-  const token = localStorage.getItem("studentToken");
-  if (!token) {
-    store.logout();
-    return;
-  }
-  try {
-    const response = await axios.post(
-      "https://aps-website-backend.onrender.com/api/v1/auth/checkToken",
-      {},
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-
-    if (response.status === 401) {
-      store.logout();
-      console.log('Unauthorized')
-    }
-  } catch (error) {
-    console.error("Token validation error:", error);
-    store.logout();
-  }
-};
-
-// Validate token when the component is mounted
-onMounted(() => {
-  validateToken();
-});
+const store = useUserStore();
+const displayFooter = computed(() => store.displayFooter);
+const displayHeader = computed(() => store.displayHeader);
 </script>
 
-
-
 <style scoped>
-@import '@/assets/index.css';
+@import "@/assets/index.css";
 </style>

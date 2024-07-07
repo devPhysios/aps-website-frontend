6e5bf2a633c20cff40f9ef4c4ffb7cf247460921
FeranmiDev <macguzik98@gmail.com>
2024-07-07 09:33:10 +0100

    refactor: Update month input to use select dropdown in Uploadbirthdays.vue

--- src/views/Birthdayevent.vue
+++ src/views/Birthdayevent.vue
@@ -7,7 +7,7 @@
     >
       <video
         ref="backgroundVideo"
-        class="w-full h-full object-fill"
+        class="w-full h-full object-cover"
         autoplay
         loop
         muted
@@ -39,6 +39,7 @@
         </svg>
       </div>
 
+      <!-- No Birthday Events Message -->
       <div
         v-else-if="birthdays.length === 0"
         class="flex flex-col items-center justify-center h-screen text-white"
@@ -49,62 +50,60 @@
         </p>
       </div>
 
-      <div v-else class="relative z-10 container mx-auto px-4 py-8">
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
-          <div
-            v-for="birthday in birthdays"
-            :key="birthday.matricNumber"
-            class="backdrop-blur-md bg-opacity-20 bg-gray-800 rounded-lg p-6 shadow-lg transition-all duration-300 hover:shadow-2xl hover:scale-105"
-          >
-            <div class="flex flex-col items-center text-white">
-              <div class="relative w-full aspect-[2/3] mb-4">
-                <img
-                  :src="birthday.imageUrl"
-                  @load="() => imageLoaded(birthday)"
-                  @error="retryImageLoad($event, birthday)"
-                  class="rounded-xl object-cover w-full h-full shadow-md"
-                  :class="{ hidden: !birthday.imageLoaded }"
-                />
-                <svg
-                  v-if="!birthday.imageLoaded"
-                  class="absolute top-0 left-0 right-0 bottom-0 m-auto h-8 w-8 animate-spin text-indigo-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    class="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    stroke-width="4"
-                  ></circle>
-                  <path
-                    class="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8v8h8a8 8 0 01-8 8V4a8 8 0 01-8 8h8v8H4a8 8 0 01-8-8 8 8 0 014-6.93z"
-                  ></path>
-                </svg>
-              </div>
-              <h2 class="text-2xl font-bold mt-2 font-poppins text-indigo-300">
-                {{ birthday.fullName }}
-              </h2>
-              <p class="text-gray-300 font-poppins">
-                {{ getMonthName(birthday.birthdayMonth) }}
-                {{ birthday.birthdayDay }}
-              </p>
-              <p class="text-gray-300 font-poppins">
-                Class Set: {{ birthday.classSet }}
-              </p>
-              <p class="text-gray-300 font-poppins">
-                {{ birthday.level }} Level
-              </p>
-              <div
-                v-html="birthday.birthdayWish"
-                class="mt-4 text-gray-300 italic font-poppins"
-              ></div>
+      <!-- Birthday Cards -->
+      <div
+        v-else
+        class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8"
+      >
+        <div
+          v-for="birthday in birthdays"
+          :key="birthday.matricNumber"
+          class="backdrop-blur-md bg-opacity-20 bg-gray-800 rounded-lg p-6 shadow-lg transition-all duration-300 hover:shadow-2xl hover:scale-105"
+        >
+          <div class="flex flex-col items-center text-white">
+            <div class="relative w-full aspect-[2/3] mb-4">
+              <img
+                :src="birthday.imageUrl"
+                alt="Birthday Image"
+                @load="() => imageLoaded(birthday)"
+                @error="retryImageLoad($event, birthday)"
+                class="rounded-xl object-cover w-full h-full shadow-md"
+                :class="{ hidden: !birthday.imageLoaded }"
+              />
+              <svg
+                v-if="!birthday.imageLoaded"
+                class="absolute top-0 left-0 right-0 bottom-0 m-auto h-8 w-8 animate-spin text-indigo-500"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  class="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  class="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8v8h8a8 8 0 01-8 8V4a8 8 0 01-8 8h8v8H4a8 8 0 01-8-8 8 8 0 014-6.93z"
+                ></path>
+              </svg>
             </div>
+            <h2 class="text-2xl font-bold mt-2 font-poppins text-indigo-300">
+              {{ birthday.fullName }}
+            </h2>
+            <p class="text-gray-300 font-poppins">
+              {{ getMonthName(birthday.birthdayMonth) }}
+              {{ birthday.birthdayDay }}
+            </p>
+            <p class="text-gray-300 font-poppins">{{ birthday.classSet }}</p>
+            <div
+              v-html="birthday.birthdayWish"
+              class="mt-4 text-gray-300 italic font-poppins"
+            ></div>
           </div>
         </div>
       </div>
@@ -129,15 +128,12 @@ const videoFiles = [
   "videos/birthday-video-1.mp4",
   "videos/birthday-video-2.mp4",
   "videos/birthday-video-3.mp4",
-  // Add paths to other videos here
 ];
-
 const musicFiles = [
   "music/happy-birthday-1.mp3",
   "music/happy-birthday-2.mp3",
   "music/happy-birthday-3.mp3",
   "music/happy-birthday-4.mp3",
-  // Add paths to other music files here
 ];
 
 const fetchBirthdays = async () => {
@@ -152,16 +148,12 @@ const fetchBirthdays = async () => {
         imageLoaded: false,
       }));
     } else {
-      setTimeout(() => {
-        window.location.href = "/";
-      }, 5000);
+      setTimeout(() => (window.location.href = "/"), 5000);
     }
   } catch (error) {
     toast.error("Error fetching birthday events. Redirecting to home page...");
     console.error("Error fetching birthdays:", error);
-    setTimeout(() => {
-      window.location.href = "/";
-    }, 5000);
+    setTimeout(() => (window.location.href = "/"), 5000);
   } finally {
     loading.value = false;
   }
@@ -182,9 +174,7 @@ const retryImageLoad = (event, birthday) => {
     birthday.retryCount++;
     console.log(`Retrying image load attempt ${birthday.retryCount}`);
     event.target.src = "";
-    setTimeout(() => {
-      event.target.src = birthday.imageUrl;
-    }, 1000);
+    setTimeout(() => (event.target.src = birthday.imageUrl), 1000);
   } else {
     console.error(
       `Image failed to load after 5 attempts: ${birthday.imageUrl}`
@@ -198,22 +188,17 @@ const selectRandomVideo = () => {
 };
 
 const playMusicSequence = () => {
-  if (audioInitialized.value || isPlaying.value) {
-    return;
-  }
-
-  if (!audioPlayer.value) {
-    audioPlayer.value = new Audio();
-  }
+  if (audioInitialized.value || isPlaying.value) return;
+  if (!audioPlayer.value) audioPlayer.value = new Audio();
 
   let currentIndex = 0;
 
   const playNextMusic = () => {
     if (currentIndex < musicFiles.length) {
       audioPlayer.value.src = musicFiles[currentIndex];
-      audioPlayer.value.play().catch((error) => {
-        console.error("Error playing audio:", error);
-      });
+      audioPlayer.value
+        .play()
+        .catch((error) => console.error("Error playing audio:", error));
       currentIndex++;
     } else {
       currentIndex = 0;
@@ -222,7 +207,6 @@ const playMusicSequence = () => {
   };
 
   audioPlayer.value.onended = playNextMusic;
-
   audioPlayer.value.src = "music/happy-birthday-default.mp3";
   audioPlayer.value
     .play()
@@ -230,9 +214,7 @@ const playMusicSequence = () => {
       audioInitialized.value = true;
       isPlaying.value = true;
     })
-    .catch((error) => {
-      console.error("Error playing audio:", error);
-    });
+    .catch((error) => console.error("Error playing audio:", error));
 };
 
 const debounce = (func, wait) => {
@@ -248,47 +230,85 @@ const debounce = (func, wait) => {
 };
 
 const initializeAudio = debounce(() => {
-  if (!audioInitialized.value && !isPlaying.value) {
-    playMusicSequence();
-  }
+  if (!audioInitialized.value && !isPlaying.value) playMusicSequence();
 }, 300);
 
 const handleVisibilityChange = () => {
-  if (document.hidden) {
-    if (audioPlayer.value) {
-      audioPlayer.value.pause();
-    }
-  } else {
-    if (audioInitialized.value && audioPlayer.value) {
-      audioPlayer.value
-        .play()
-        .catch((error) => console.error("Error resuming audio:", error));
-    }
-  }
+  if (document.hidden && audioPlayer.value) audioPlayer.value.pause();
+  else if (!document.hidden && audioInitialized.value) audioPlayer.value.play();
 };
 
-onMounted(() => {
-  fetchBirthdays().then(() => {
-    if (birthdays.value.length > 0 && backgroundVideo.value) {
-      backgroundVideo.value.src = selectRandomVideo();
-    }
-    window.addEventListener("click", initializeAudio);
-    document.addEventListener("visibilitychange", handleVisibilityChange);
-  });
+onMounted(async () => {
+  await fetchBirthdays();
+  loading.value = false;
+  const videoSrc = selectRandomVideo();
+  backgroundVideo.value.src = videoSrc;
+  window.addEventListener("click", initializeAudio);
+  document.addEventListener("visibilitychange", handleVisibilityChange);
 });
 
 onUnmounted(() => {
   window.removeEventListener("click", initializeAudio);
   document.removeEventListener("visibilitychange", handleVisibilityChange);
-
   if (audioPlayer.value) {
     audioPlayer.value.pause();
-    audioPlayer.value.src = "";
     audioPlayer.value = null;
   }
 });
 </script>
 
 <style scoped>
-/* Add your styles here */
+audio-init-button {
+  display: none;
+}
+
+.container {
+  padding-top: 4rem;
+  padding-bottom: 4rem;
+}
+
+.birthday-cards-container {
+  display: grid;
+  grid-template-columns: repeat(1, minmax(0, 1fr));
+  gap: 2rem;
+}
+
+@media (min-width: 640px) {
+  .birthday-cards-container {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+}
+
+@media (min-width: 1024px) {
+  .birthday-cards-container {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+  }
+}
+
+.spinner-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.message-container {
+  text-align: center;
+}
+
+h1 {
+  font-size: 1.875rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+}
+
+p {
+  font-size: 1.125rem;
+}
+
+@media (min-width: 640px) {
+  h1 {
+    font-size: 2.25rem;
+  }
+}
 </style>

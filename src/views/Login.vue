45f899473296e4e268f3895709437e1e6619fd8f
FeranmiDev <macguzik98@gmail.com>
2025-05-30 09:06:00 +0100

    fix: Update Login.vue to replace <p> tags with <a> tags for better semantics and accessibility in password reset and login prompts

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,15 @@
 <template>
     <div v-if="loginComponent" class="pt-5 font-display">
         <StudentLogin />
-        <p class="text-center mt-[-185px] pb-48 text-1xl">
-            <p @click="resetPassword" class="text-blue-500 hover:underline">Forgot Password?</p>
-        </p>
+        <div class="text-center mt-[-185px] pb-48 text-1xl">
+            <a @click="resetPassword" class="text-blue-500 hover:underline cursor-pointer">Forgot Password?</a>
+        </div>
     </div>
     <div v-if="!loginComponent">
         <ResetPassword />
-        <p class="text-center text-1xl mt-[-120px]" >
-            <p @click="login" class="text-blue-500 hover:underline">Do you want to login?</p>
-        </p>
+        <div class="text-center text-1xl mt-[-120px]">
+            <a @click="login" class="text-blue-500 hover:underline cursor-pointer">Do you want to login?</a>
+        </div>
     </div>
 </template>
 <script setup>
